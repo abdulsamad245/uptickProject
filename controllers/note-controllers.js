e68d5a077b688fa42156a8117364559d8e146fcb
abdulsamad245 <100769627+abdulsamad245@users.noreply.github.com>
2023-03-01 23:00:20 +0100

    Update note-controllers.js

--- controllers/note-controllers.js
+++ controllers/note-controllers.js
@@ -6,15 +6,6 @@ const url = require('url');
 
 module.exports.createNote = function (req, res, next) {
     const user = req.user;
-    // Color.findById(req.body.color, function (err, color) {
-    //     if (err)
-    //         return res.status(400).json({errors: err});
-    //     if(!color)
-    //         return res.status(400).json({color: "No color found"});
-
-    //     if (!user._id.equals(color.user))
-    //         return res.status(400).json({color: "Can't find such color in your colors list. " +
-    //                 "Either create it of choose another one"});
         Note.create({
             title: req.body.title,
             text: req.body.text,
@@ -25,20 +16,9 @@ module.exports.createNote = function (req, res, next) {
                 return res.status(400).json({errors: err});
             res.json({message: 'Note created successfully'})
         });
-    // });
 };
 
 module.exports.getNotesList = function (req, res, next) {
-    // let queryObject = url.parse(req.url,true).query;
-    // console.log(queryObject);
-    // Object.keys(queryObject).forEach(function (key, obj) {
-    //     if (key === 'q'){
-    //         queryObject.$text = {$search: queryObject[key]};
-    //     }
-    //     if (!Note.schema.paths.hasOwnProperty(key)){
-    //         delete queryObject[key];
-    //     }
-    // });
     Note.find({user: req.user, isActive: true})
         .sort('-created').exec(function (err, notes) {
        if (err)
@@ -92,12 +72,3 @@ module.exports.getAllNotes = function (req, res, next) {
     });
 };
 
-// module.exports.deactivateNote = function (req, res, next) {
-//     Note.findByIdAndUpdate(req.params.id, {isActive: false}, {new: true}, function (err, note) {
-//         if (err)
-//             return res.status(500).json({errors: err});
-//         if (!note)
-//             return res.status(404).json({note: "Note not found"});
-//         res.json({note: note, message: "Mark as Done!"});
-//     });
-// }
