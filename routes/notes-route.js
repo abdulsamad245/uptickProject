9950210afc29cc2333d6f2b2529f04cf0101bd18
abdulsamad245 <100769627+abdulsamad245@users.noreply.github.com>
2023-03-02 00:15:29 +0100

    Update notes-route.js

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -5,8 +5,6 @@ const { check, validationResult } = require('express-validator');
 const verifyToken = require('../auth/VerifyToken');
 
 router.post('/note', verifyToken, controllers.createNote);
-router.get('/note/list', verifyToken, controllers.getAllNotes);
-router.get('/note', verifyToken, controllers.getNotesList);
 router.get('/note/:id', verifyToken, controllers.getNoteByID);
 router.put('/note/:id', verifyToken, controllers.updateNoteById);
 router.delete('/note/:id', verifyToken, controllers.deleteNoteById);
